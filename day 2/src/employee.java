86f8b78bcdac4bc3820e6e397a211ecc161430de
Elamathi8199 <70468010+Elamathi8199@users.noreply.github.com>
2020-10-14 12:26:39 +0530

    Add files via upload

---
+++ day 2/src/employee.java
@@ -0,0 +1,11 @@
+public class employee {
+   public String name;
+   public short age;
+   public String city;
+
+   public void display(){
+       System.out.println("The name is "+name);
+       System.out.println("Age is "+age);
+       System.out.println("City is "+city);
+   }
+}
